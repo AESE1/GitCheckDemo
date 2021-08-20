c39b77bbad18c5ed0230ba1d095da00eff8c35e5
Anton.Ellis <Anton.Ellis2@outlook.com>
2021-08-20 14:15:20 +0100

    updates

--- gitcheck.sh
+++ gitcheck.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-REPO=GitCheckDemo
+REPO=GardeningProject
+
+if [ ! -d "$REPO" ]; then
+git clone https://github.com/AESE1/GardeningProject.git
+else
+git fetch https://github.com/AESE1/GardeningProject.git
+fi
 
